ace0affaeb46cc85931d4ebc99b92d057699b5ce
raoyongqi <1152416230@qq.com>
2024-10-08 15:24:41 +0800

    first commit

---
+++ getfans.ps1
@@ -0,0 +1,20 @@
+function Get-BilibiliFans {
+    param (
+        [string]$uid  # 传入的 UP 主 ID
+    )
+
+    # 发送请求到 Bilibili API 获取粉丝数据
+    $url = "https://api.bilibili.com/x/relation/stat?vmid=$uid"
+    $response = Invoke-RestMethod -Uri $url -Method Get
+
+    # 检查是否返回数据并获取粉丝数量
+    if ($response.code -eq 0) {
+        $fans = $response.data.follower
+        Write-Output "UP 主 $uid 的粉丝数量是: $fans"
+    } else {
+        Write-Output "无法获取 UP 主的粉丝数据。错误代码: $($response.code)"
+    }
+}
+
+# 示例调用: 使用 Bilibili 上某个 UP 主的 UID
+Get-BilibiliFans -uid 517327498  # 请将 '12345678' 替换为实际的 UP 主 UID
---
+++ readme.md
@@ -0,0 +1,5 @@
+# 多多poi丶个人主页-哔哩哔哩视频
+https://space.bilibili.com/11253297/
+
+# 罗翔说刑法
+https://space.bilibili.com/517327498/
